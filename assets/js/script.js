557bd72c7c995a7194c74c786c2238534fda994c
salahahmed2001 <salah01068441@gmail.com>
2024-04-02 04:15:27 +0200

    add new html and css style and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,15 @@
 let BtnTogle = document.querySelector(".nav-toggle-btn");
 let Menu = document.querySelector(".link-list");
 let VewDetails = document.querySelector(".btn-img");
+let BtnSignIn = document.querySelector(".btn-sign-in");
+let cards = document.querySelector(".cards");
+let SignIn = document.querySelector(".displaynone");
+let SignUp = document.querySelector(".displaynone-up");
+let BtnSignUp = document.querySelector(".btn-sign-up");
+let ShowAds = document.querySelector(".ads");
+let BtnAds = document.querySelector(".icon-ads");
+let BtnSignInNone = document.querySelector(".icon-sign-in");
+let BtnSignUpNone = document.querySelector(".icon-sign-up");
 
 BtnTogle.addEventListener("click", () => {
   Menu.classList.toggle("active-list");
@@ -10,3 +19,33 @@ BtnTogle.addEventListener("click", () => {
 VewDetails.addEventListener("click", function () {
   window.location = "../../index-details-hell.html";
 });
+BtnSignIn.addEventListener("click", () => {
+  cards.style.display = "none";
+  SignIn.style.display = "flex";
+});
+
+BtnSignUp.addEventListener("click", () => {
+  cards.style.display = "none";
+  SignUp.style.display = "flex";
+});
+setInterval(() => {
+  cards.style.display = "none";
+  ShowAds.style.display = "flex";
+}, 300000);
+
+BtnAds.addEventListener("click", () => {
+  cards.style.display = "flex";
+  ShowAds.style.display = "none";
+});
+
+BtnSignInNone.addEventListener("click", () => {
+  cards.style.display = "flex";
+  SignIn.style.display = "none";
+});
+
+BtnSignUpNone.addEventListener("click", () => {
+  cards.style.display = "flex";
+  SignUp.style.display = "none";
+});
+// overflow: hidden;
+// display: flex;
