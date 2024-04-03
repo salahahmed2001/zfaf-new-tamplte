60b8f8b66a0a3f0a29f316dd9e662fe4b351afaa
salahahmed2001 <salah01068441@gmail.com>
2024-04-03 07:00:50 +0200

    add new html and js and css style

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,42 +10,137 @@ let ShowAds = document.querySelector(".ads");
 let BtnAds = document.querySelector(".icon-ads");
 let BtnSignInNone = document.querySelector(".icon-sign-in");
 let BtnSignUpNone = document.querySelector(".icon-sign-up");
+let VewHotels = document.querySelector(".hotels");
+let VewHalls = document.querySelector(".halls");
+let Planning = document.querySelector(".planning");
+let Individuals = document.querySelector(".individuals");
+let Detls = document.querySelector(".detls");
+let BtnNext = document.querySelector(".next");
+let PlanningGrob = document.querySelector(".planning-grob");
+let BtnNextContact = document.querySelector(".next-contact");
+let BtnBack = document.querySelector(".back");
+let Contact = document.querySelector(".contact");
+let BtnBackContact = document.querySelector(".back-detls");
+let Send = document.querySelector(".send");
 
 BtnTogle.addEventListener("click", () => {
   Menu.classList.toggle("active-list");
   BtnTogle.classList.toggle("active");
 });
 
-VewDetails.addEventListener("click", function () {
-  window.location = "../../index-details-hell.html";
-});
 BtnSignIn.addEventListener("click", () => {
-  cards.style.display = "none";
   SignIn.style.display = "flex";
 });
 
 BtnSignUp.addEventListener("click", () => {
-  cards.style.display = "none";
   SignUp.style.display = "flex";
-});
-setInterval(() => {
-  cards.style.display = "none";
-  ShowAds.style.display = "flex";
-}, 300000);
-
-BtnAds.addEventListener("click", () => {
-  cards.style.display = "flex";
-  ShowAds.style.display = "none";
 });
 
 BtnSignInNone.addEventListener("click", () => {
-  cards.style.display = "flex";
   SignIn.style.display = "none";
 });
 
 BtnSignUpNone.addEventListener("click", () => {
-  cards.style.display = "flex";
   SignUp.style.display = "none";
 });
-// overflow: hidden;
-// display: flex;
+setInterval(() => {
+  // cards.style.display = "none";
+  ShowAds.style.display = "flex";
+}, 300000);
+
+BtnAds.addEventListener("click", () => {
+  // cards.style.display = "flex";
+  ShowAds.style.display = "none";
+});
+VewDetails.addEventListener("click", function () {
+  window.location = "../../index-details-hell.html";
+});
+
+VewHalls.addEventListener("click", () => {
+  Planning.style.right = "-3000px";
+  Individuals.style.left = "0px";
+  PlanningGrob.style.height = "150vh";
+});
+
+VewHotels.addEventListener("click", () => {
+  Planning.style.right = "-3000px";
+  Individuals.style.left = "0px";
+  PlanningGrob.style.height = "140vh";
+});
+BtnNext.addEventListener("click", () => {
+  if (
+    document.querySelector(".radio1").checked ||
+    document.querySelector(".radio2").checked ||
+    document.querySelector(".radio3").checked ||
+    document.querySelector(".radio4").checked ||
+    document.querySelector(".radio5").checked ||
+    document.querySelector(".radio6").checked ||
+    document.querySelector(".radio7").checked
+  ) {
+    Individuals.style.display = "none";
+    Detls.style.left = "0px";
+    PlanningGrob.style.height = "90vh";
+  } else {
+    document.querySelector(".warning-radio").innerHTML = "برجاء اختيار سعر ";
+  }
+});
+BtnBack.addEventListener("click", () => {
+  setTimeout(() => {
+    Individuals.style.display = "flex";
+  }, 200);
+  Individuals.style.right = "0px";
+  Detls.style.left = "-150%";
+  PlanningGrob.style.height = "140vh";
+});
+BtnNextContact.addEventListener("click", () => {
+  let Textarea = document.querySelector("textarea");
+  if (Textarea.value != "") {
+    Detls.style.display = "none";
+    Contact.style.left = "0px";
+    PlanningGrob.style.height = "120vh";
+  } else {
+    document.querySelector(".warning-text").innerHTML =
+      "لا يمكن ترك الحقل فارغ";
+  }
+});
+BtnBackContact.addEventListener("click", () => {
+  setTimeout(() => {
+    Detls.style.display = "flex";
+  }, 200);
+  Detls.style.right = "0px";
+  Contact.style.left = "-100%";
+  PlanningGrob.style.height = "90vh";
+});
+Send.addEventListener("click", () => {
+  let InputName = document.querySelector("input[name=text]");
+  let InputEmail = document.querySelector("input[name=email]");
+  let InputPhone = document.querySelector("input[name=phone]");
+  let InputDate = document.querySelector("input[name=date]");
+  let InputCity = document.querySelector("input[name=city]");
+  let checkbox = document.querySelector("label[for=checkbox]");
+  if (InputName.value != "") {
+  } else {
+    document.querySelector(".warning-nameall").innerHTML = "برجاء كتابة الاسم";
+  }
+  if (InputEmail.value != "") {
+  } else {
+    document.querySelector(".warning-email").innerHTML =
+      "برجاء كتابة البريد الالكتروني";
+  }
+  if (InputPhone.value != "") {
+  } else {
+    document.querySelector(".warning-tel").innerHTML = "برجاء كتابة رقم الهاتف";
+  }
+  if (InputDate.value != "") {
+  } else {
+    document.querySelector(".warning-date").innerHTML = "يجب ادخال موعد الزفاف";
+  }
+  if (InputCity.value != "") {
+  } else {
+    document.querySelector(".warning-city").innerHTML = "يجب ادخال المدينة";
+  }
+  if (document.querySelector("label[for=checkbox]").checked) {
+  } else {
+    checkbox.style.color = "red";
+  }
+});
